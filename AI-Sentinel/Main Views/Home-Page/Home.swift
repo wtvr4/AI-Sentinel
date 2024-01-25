0258c567fb74c441777150c426cbea1a233fc232
wtvr4 <wtvr44@gmail.com>
2024-01-25 12:22:58 +0400

    some work on home page

--- AI-Sentinel/Main Views/Home-Page/Home.swift
+++ AI-Sentinel/Main Views/Home-Page/Home.swift
@@ -2,16 +2,33 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        ZStack{
+        ZStack {
             Color("background")
                 .ignoresSafeArea()
-        
-        VStack{
-                
-                Text("Home")
             
+            VStack(spacing: 3) {
+                Text("Hello, Mohamed")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+                    .offset(x: -96, y: 23)
+                Text("Welcome to AI Sentinel")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                    .offset(x: -65, y: 23)
+                Spacer()
+                
+                Text("Recent Updates")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                
+                Spacer()
             }
+            .padding()
         }
+        
     }
 }
 
